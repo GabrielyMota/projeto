ab6d7d8c85cec1185b362b8599a21dfabc4b4d1d
GabrielyMota <gabrielymota26@gmail.com>
2022-02-07 13:43:22 -0400

    tela login e cadastro

--- main.js
+++ main.js
@@ -1,20 +1,8 @@
-// function handleCredentialResponse(response) {
-//     const data = jwt_decode(response.credential);
-// }
-// window.onload = function () {
-//     google.accounts.id.initialize({
-//         client_id: "264399802675-1mq3uchsicog31tq9f4uc71vo2m09gpd.apps.googleusercontent.com",
-//         callback: handleCredentialResponse
-//     });
-//     google.accounts.id.renderButton(
-//         document.getElementById("buttonDiv"),
-//         { theme: "filled_black", 
-//         size: "large", 
-//         type: "standard",
-//         shape: "pill",
-//         text: "$ {button.text}",
-//         logo_alignment: "left",
-//         width: "466"}  // customization attributes
-//     );
-//     google.accounts.id.prompt(); // also display the One Tap dialog
-// }
+function mostrarSenha(){
+    var senha = document.getElementById('mostrar');
+    if(senha.type="password"){
+        senha.type ="text";
+    }else{
+        senha.type = "password";
+    }
+}
